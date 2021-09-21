aca72ba014769198733aa92da7fe1194c46a7a59
Siraj <sirajkhanc2305@gmail.com>
2021-09-21 22:59:57 +0530

    NYSE changed to BSE

--- src/main/java/com/bridgelabz/StockMain.java
+++ src/main/java/com/bridgelabz/StockMain.java
@@ -16,7 +16,8 @@ import java.util.Scanner;
 public class StockMain {
     public static void main(String[] args) throws IOException, InterruptedException {
         String apiKey = "UFR1P6WOBTSIH4J8";
-        System.out.println("Welcome to the NYSE stock market real time data monitor");
+        System.out.println("Welcome to the BSE stock market real time stock price monitor");
+        Thread.sleep(1500);
         System.out.println("NOTE : You need to create an API key for this program to work. The default API Key included in this program can only work 5 times a day");
         System.out.print("To use the default API key press 1 (may not work depending on number of usage per day) or To create a new API key press 2 : ");
         if (new Scanner(System.in).nextInt() == 2) {
@@ -39,9 +40,9 @@ public class StockMain {
     }
 
     public static void apiConnection(String apiKey) throws IOException, InterruptedException {
-        System.out.print("Enter NYSE symbol of the stock to know the details : ");
+        System.out.print("Enter BSE symbol of the stock to know the details : ");
         String symbol = new Scanner(System.in).nextLine().toUpperCase();
-        var url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=60min&apikey=" + apiKey;
+        var url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + symbol + ".BSE&outputsize=full&apikey=" + apiKey + "";
         var request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
         var client = HttpClient.newBuilder().build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -49,15 +50,20 @@ public class StockMain {
         JsonObject object = gson.fromJson(response.body(), JsonObject.class);
         String refresh = object.get("Meta Data").getAsJsonObject().get("3. Last Refreshed").getAsString();
         try {
-            System.out.println("\nAll the stock details of " + symbol + " is listed below for the last market open date " + refresh.substring(0,10));
-            System.out.println("\nOpening price -> " + object.get("Time Series (60min)").getAsJsonObject().get(refresh).getAsJsonObject().get("1. open"));
-            System.out.println("Closing price -> " + object.get("Time Series (60min)").getAsJsonObject().get(refresh).getAsJsonObject().get("4. close"));
-            System.out.println("High price -> " + object.get("Time Series (60min)").getAsJsonObject().get(refresh).getAsJsonObject().get("2. high"));
-            System.out.println("Low price -> " + object.get("Time Series (60min)").getAsJsonObject().get(refresh).getAsJsonObject().get("3. low"));
-            System.out.println("Volumes -> " + object.get("Time Series (60min)").getAsJsonObject().get(refresh).getAsJsonObject().get("5. volume"));
-        }catch (NullPointerException e)
-        {
+            System.out.println("\nAll the stock details of " + symbol + " is listed below for the last market open date " + refresh.substring(0, 10));
+            System.out.println("\nOpening price -> " + object.get("Time Series (Daily)").getAsJsonObject().get(refresh).getAsJsonObject().get("1. open"));
+            System.out.println("Closing price -> " + object.get("Time Series (Daily)").getAsJsonObject().get(refresh).getAsJsonObject().get("4. close"));
+            System.out.println("High price -> " + object.get("Time Series (Daily)").getAsJsonObject().get(refresh).getAsJsonObject().get("2. high"));
+            System.out.println("Low price -> " + object.get("Time Series (Daily)").getAsJsonObject().get(refresh).getAsJsonObject().get("3. low"));
+            System.out.println("Volumes -> " + object.get("Time Series (Daily)").getAsJsonObject().get(refresh).getAsJsonObject().get("6. volume"));
+        } catch (NullPointerException e) {
             System.out.println("Sorry the given stock symbol isn't registered with the API");
+        }
+        System.out.println("To exit the program press 2 else press any key");
+        {
+            if (new Scanner(System.in).nextInt() == 2) {
+                System.exit(1);
+            } else apiConnection(apiKey);
         }
     }
 }
